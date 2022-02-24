294bc4940bca3b545cad764ccb8d97ec6a0d8504
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2022-02-24 01:59:30 -0500

    :art::boom: Main modules created, favicon and styles updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from '@app/app-routing.module';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
+import { CoreModule } from '@core/core.module';
 import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from '@app/app.component';
@@ -26,6 +27,7 @@ import { environment } from '@src/environments/environment';
       logOnly: environment.production
     }),
     EffectsModule.forRoot([]),
+    CoreModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
       preventDuplicates: true
