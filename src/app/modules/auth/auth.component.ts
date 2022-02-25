75b0a6502c6e966be8cf761dd15d3a5b1de66452
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2022-02-25 17:14:53 -0500

    :construction: Auth module main component in progress

--- src/app/modules/auth/auth.component.ts
+++ src/app/modules/auth/auth.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DEFAULT_SCROLLBAR_OPTIONS, ScrollbarOptions } from '@models/scrollbar';
+import { AuthType } from '@modules/auth/models/auth';
 
 @Component({
   selector: 'mi-auth',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class AuthComponent {
+  readonly scrollbarOptions: ScrollbarOptions = {
+    ...DEFAULT_SCROLLBAR_OPTIONS,
+    overflowBehavior: {
+      x: 'visible-hidden'
+    }
+  };
+  readonly authType = AuthType;
+  auth = this.authType.Login;
+  loading = false;
+
   constructor() { }
 }
--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { NgxTrimDirectiveModule } from 'ngx-trim-directive';
 import { DirectivesModule } from '@directives/directives.module';
 import { IconsModule } from '@shared/icons/icons.module';
+import { OverlayscrollbarsModule } from 'overlayscrollbars-ngx';
 
 import { AuthComponent } from '@modules/auth/auth.component';
 import { LoginComponent } from '@modules/auth/login/login.component';
@@ -22,7 +23,8 @@ import { RegisterComponent } from '@modules/auth/register/register.component';
     ReactiveFormsModule,
     NgxTrimDirectiveModule,
     DirectivesModule,
-    IconsModule
+    IconsModule,
+    OverlayscrollbarsModule
   ]
 })
 export class AuthModule { }
