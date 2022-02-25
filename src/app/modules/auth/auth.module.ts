339df2cd67fcf4b1bd94f3a63e4fcbd6159b49f0
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2022-02-25 15:08:33 -0500

    :construction: Auth module login component in progress

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from '@modules/auth/auth-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxTrimDirectiveModule } from 'ngx-trim-directive';
+import { DirectivesModule } from '@directives/directives.module';
+import { IconsModule } from '@shared/icons/icons.module';
 
 import { AuthComponent } from '@modules/auth/auth.component';
 import { LoginComponent } from '@modules/auth/login/login.component';
@@ -14,7 +18,11 @@ import { RegisterComponent } from '@modules/auth/register/register.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    NgxTrimDirectiveModule,
+    DirectivesModule,
+    IconsModule
   ]
 })
 export class AuthModule { }
--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,16 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'mi-login',
   templateUrl: './login.component.html',
-  styles: [
-  ]
+  styles: []
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  private readonly loadingSubject = new BehaviorSubject(false);
+  readonly loading$ = this.loadingSubject.asObservable();
 
-  constructor() { }
+  @Output()
+  loading = new EventEmitter<boolean>(false);
 
-  ngOnInit(): void {
+  readonly loginForm = this.formBuilder.group({
+    usernameOrEmail: [null],
+    password: [null]
+  });
+
+  get usernameOrEmail() {
+    return this.loginForm.get('usernameOrEmail') as FormControl;
   }
 
+  get password() {
+    return this.loginForm.get('password') as FormControl;
+  }
+
+  constructor(private formBuilder: FormBuilder) {
+    this.usernameOrEmail?.setValidators([Validators.required]);
+    this.password?.setValidators([Validators.required]);
+  }
+
+  login() {
+    this.setLoading(true);
+  }
+
+  private setLoading(loading: boolean) {
+    this.loadingSubject.next(loading);
+    this.loading.emit(loading);
+  }
 }
